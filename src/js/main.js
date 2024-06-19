394b19ea87b13106b5bfb157ad645d337ebb7782
Arnary <anastasia.dushka1@gmail.com>
2024-06-19 22:33:59 +0300

    fix empty search & unnecessary request

--- src/js/main.js
+++ src/js/main.js
@@ -9,7 +9,7 @@ const form = document.querySelector(".search-form");
 const gallery = document.querySelector(".gallery");
 const guard = document.querySelector(".js-guard");
 
-form.addEventListener("input", (event) => {query = event.target.value;});
+form.addEventListener("input", (event) => {query = event.target.value.trim();});
 form.addEventListener("submit", handlerSubmit);
 
 let page = 1;
@@ -24,71 +24,90 @@ const observer = new IntersectionObserver(handlerLoadMore, options);
 const lightbox = new SimpleLightbox('.gallery a');
 
 function handlerSubmit(event) {
-    event.preventDefault();
-    observer.unobserve(guard);
-    page = 1;
+  event.preventDefault();
+  observer.unobserve(guard);
+  page = 1;
 
-    getPhoto(query, page).then((data) => {
-      if (data.totalHits === 0) {
-            gallery.innerHTML = "";
-            
-            iziToast.error({
-            position: "topRight",
-            message: "Sorry, there are no images matching your search query. Please try again.",
-            backgroundColor: "rgb(245, 156, 22)",
-            maxWidth: "400px",
-            timeout: 3000,
-            messageColor: "black",
-            });
-            return;
-      };
+  if (!query) {
+    gallery.innerHTML = "";
+    iziToast.info({
+      position: "topRight",
+      message: "You should enter a query in the search field.",
+      maxWidth: "400px",
+      timeout: 2000,
+      messageColor: "black",
+    });
+    return;
+  }; 
 
-      iziToast.success({
-          position: "topRight",
-          message: `Hooray! We found ${data.totalHits} images.`,
-          timeout: 2000,
+  getPhoto(query, page).then((data) => { 
+    if (data.totalHits === 0) {
+      gallery.innerHTML = "";
+      iziToast.error({
+        position: "topRight",
+        message: "Sorry, there are no images matching your search query. Please try again.",
+        backgroundColor: "rgb(245, 156, 22)",
+        maxWidth: "400px",
+        timeout: 3000,
+        messageColor: "black",
+      });
+      return;
+    };
+
+    iziToast.success({
+        position: "topRight",
+        message: `Hooray! We found ${data.totalHits} images.`,
+        timeout: 2000,
+        messageColor: "black",
+    });
+
+    gallery.innerHTML = createGallery(data.hits);
+    
+    lightbox.refresh();
+    
+    window.scrollTo(0, 0);
+    
+    observer.observe(guard);
+
+    if (page*40 >= data.totalHits) {
+      iziToast.info({
+          position: "bottomCenter",
+          message: "We're sorry, but you've reached the end of search results.",
           messageColor: "black",
       });
-
-      gallery.innerHTML = createGallery(data.hits);
-      
-      lightbox.refresh();
-      
-      window.scrollTo(0,0);
-
-      observer.observe(guard);
-    }).catch((err) => {
+      observer.unobserve(guard);
+    };
+  }).catch((err) => {
         console.log(err);
     });
 };
 
 function createGallery(arr) {
-    return arr.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => 
-        `<div class="photo-card">
-        <a class="gallery-link" href="${largeImageURL}">
-        <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-        </a>
-        <div class="info">
-          <p class="info-item">
-            <b>Likes</b>
-            ${likes}
-          </p>
-          <p class="info-item">
-            <b>Views</b>
-            ${views}
-          </p>
-          <p class="info-item">
-            <b>Comments</b>
-            ${comments}
-          </p>
-          <p class="info-item">
-            <b>Downloads</b>
-            ${downloads}
-          </p>
-        </div>
-      </div>`
-
-    ).join("")
+  return arr.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) =>
+    `<div class="photo-card">
+      <a class="gallery-link" href="${largeImageURL}">
+      <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+      </a>
+      <div class="info">
+        <p class="info-item">
+          <b>Likes</b>
+          ${likes}
+        </p>
+        <p class="info-item">
+          <b>Views</b>
+          ${views}
+        </p>
+        <p class="info-item">
+          <b>Comments</b>
+          ${comments}
+        </p>
+        <p class="info-item">
+          <b>Downloads</b>
+          ${downloads}
+        </p>
+      </div>
+    </div>`
+  ).join("");
 };
 
 function handlerLoadMore(entries) {
